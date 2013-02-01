353b1fff887029f9a36ab38431a0884ff6729917
Wai Lee Chin Feman <wchinfeman@8thlight.com>
2013-02-01 16:03:29 -0600

    real first test

--- lib/time_helper.rb
+++ lib/time_helper.rb
@@ -1,8 +1,19 @@
 class TimeHelper
-  def self.record(require_time)
-    File.open("spec_helper_times.yml", 'a+') do |file|
-      date_time = DateTime.now.strftime("%m%d%y%H%M")
+  FILENAME = "spec_helper_times.yml"
+  def self.record_require_time(require_time)
+    File.open(FILENAME, 'a+') do |file|
+      date_time = Clock.now
       file << "#{date_time}: #{require_time}\n"
     end
+  end
+end
+
+class Clock
+  def self.now
+    serialize(DateTime.now)
+  end
+
+  def self.serialize(date_time)
+    date_time.strftime("%m%d%y%H%M")
   end
 end
--- spec/time_helper_spec.rb
+++ spec/time_helper_spec.rb
@@ -1,7 +1,15 @@
 require 'time_helper'
 
 describe TimeHelper do
-  it "foo" do
-    TimeHelper.record(100)
+  before :each do
+    if File.exist?(TimeHelper::FILENAME)
+      File.delete(TimeHelper::FILENAME)
+    end
+  end
+
+  it "records the time according to clock output" do
+    TimeHelper.record_require_time(100)
+    datas = YAML.load(open(TimeHelper::FILENAME))
+    datas[datas.keys.first].should == 100
   end
 end
